d4a179e8c4a966a0716c751ceafe71b794fb7317
priyansh19csu225 <71059426+priyansh19csu225@users.noreply.github.com>
2023-09-08 19:19:34 +0530

    some more changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
-const router = require('./route/route');
+const router = require('./src/route/route');
 
 const app = express();
 
